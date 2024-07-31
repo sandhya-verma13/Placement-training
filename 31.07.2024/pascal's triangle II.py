0b294a413ad203305425749a41490feb3465395a
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2024-07-31 20:05:21 +0530

    Create pascal's triangle II.py

---
+++ 31.07.2024/pascal's triangle II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        n=20
+        s=[]
+        for i in range(1,n+1):
+            a=1
+            y=[]
+            for j in range(1,i+1):
+                y.append(a)
+                a=a*(i-j)//j
+                print(a)
+            s.append(y)
+        return s[rowIndex]
