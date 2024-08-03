cf4e5578fb8e5a1204dfeb243cc6218353f605ca
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2024-08-03 19:40:00 +0530

    Create pattern2.py

---
+++ 02.08.2024/pattern2.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the number of row : "))
+k=0
+for i in range(1,n+1):
+    for j in range(1,(n-i)+1):
+        print(end=" ")
+    while k!=(2*i-1):
+        print("*",end="")
+        k+=1
+    k=0
+    print()
+        
