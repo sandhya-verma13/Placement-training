24296f73c0bf2839a42697900b087cc74fad9f1e
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2024-08-08 21:10:52 +0530

    Create Area of rectangle.c

---
+++ 08.08.2024/Area of rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int l,w,a;
+    printf("Enter the length of the rectangle : ");
+    scanf("%d",&l);
+    printf("Enter the Width of the rectangle : ");
+    scanf("%d",&w);
+    a=l*w;
+    printf("The Area of the rectangle is : %d", a);
+    
+}
